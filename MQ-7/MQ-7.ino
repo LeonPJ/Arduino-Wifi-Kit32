ced3e1e85b11803278dd7e9a41237f9e5c51b507
LeonPJ <leon19941214@gmail.com>
2018-08-09 16:33:30 +0800

    verify

--- MQ-7/MQ-7.ino
+++ MQ-7/MQ-7.ino
@@ -1,18 +1,13 @@
-void setup() 
-{
+#include "MQ7.h"
+
+MQ7 mq7(13, 5.0); // (pinMode, Vcc)
+
+void setup() {
   Serial.begin(9600);
 }
- 
-void loop() 
-{
-  float sensorVoltage; 
-  float sensorValue;
- 
-  sensorValue = analogRead(13);
-  sensorVoltage = sensorValue/1024*5.0;
- 
-  Serial.print("sensor voltage = ");
-  Serial.print(sensorVoltage);
-  Serial.println(" V");
+
+void loop() {
+  String stringCOPPM = String("CO: ") + String(mq7.getPPM()) + String(" ppm");
+  Serial.println(stringCOPPM);
   delay(1000);
 }
